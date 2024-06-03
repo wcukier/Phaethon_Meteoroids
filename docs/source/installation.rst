6048d62f6755d62002e8ca2b1ef98d440c6de280
Wolf Cukier <wcukier@princeton.edu>
2024-06-03 15:45:44 -0400

    code_reqs and kernaels

--- docs/source/installation.rst
+++ docs/source/installation.rst
@@ -28,7 +28,7 @@ These packages can be installed by running the command
 
 .. code-block:: bash
 
-    pip install -r requirements.txt
+    pip install -r code_reqs.txt
 
 from the project folder.
 
@@ -47,9 +47,9 @@ Running the file downloadKernals.sh should download all kernals not included in 
 Note: Some of the links have changed so these dropbox links should have everything
 necessary.  Email wolfcukier AT gmail... if the links fail.
 
-small kernels: https://www.dropbox.com/scl/fi/ayk4384rjqr9loa7g6vcu/small_kernels.zip?rlkey=iei7x40aelfo5cuv2cqm0xygx&dl=1
-de431_part-1: https://www.dropbox.com/scl/fi/ll2jjh7bnr3vz7m2yk9qd/de431_part-1.bsp?rlkey=gl565mjj0waoas2i0shu3fa32&dl=1
-de431_part-2: https://www.dropbox.com/scl/fi/dadjwyn5u70ggxi4fdvip/de431_part-2.bsp?rlkey=ryoepyjg8dyic3s2drl7lvwn1&dl=1
+ - small kernels: https://www.dropbox.com/scl/fi/ayk4384rjqr9loa7g6vcu/small_kernels.zip?rlkey=iei7x40aelfo5cuv2cqm0xygx&dl=1
+ - de431_part-1: https://www.dropbox.com/scl/fi/ll2jjh7bnr3vz7m2yk9qd/de431_part-1.bsp?rlkey=gl565mjj0waoas2i0shu3fa32&dl=1
+ - de431_part-2: https://www.dropbox.com/scl/fi/dadjwyn5u70ggxi4fdvip/de431_part-2.bsp?rlkey=ryoepyjg8dyic3s2drl7lvwn1&dl=1
 
 
 Output Directories
--- downloadKernals.sh
+++ downloadKernals.sh
@@ -1,17 +1,21 @@
 
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20210226_20210325_v001.bsp --output geminids/data/SPICE/spp_recon_20210226_20210325_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20210101_20210226_v001.bsp --output geminids/data/SPICE/spp_recon_20210101_20210226_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200802_20201016_v001.bsp --output geminids/data/SPICE/spp_recon_20200802_20201016_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20201016_20210101_v001.bsp --output geminids/data/SPICE/spp_recon_20201016_20210101_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200705_20200802_v001.bsp --output geminids/data/SPICE/spp_recon_20200705_20200802_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200505_20200705_v001.bsp --output geminids/data/SPICE/spp_recon_20200505_20200705_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200301_20200505_v001.bsp --output geminids/data/SPICE/spp_recon_20200301_20200505_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200101_20200301_v001.bsp --output geminids/data/SPICE/spp_recon_20200101_20200301_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20190914_20200101_v001.bsp --output geminids/data/SPICE/spp_recon_20190914_20200101_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20190416_20190914_v001.bsp --output geminids/data/SPICE/spp_recon_20190416_20190914_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20190120_20190416_v001.bsp --output geminids/data/SPICE/spp_recon_20190120_20190416_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20181008_20190120_v001.bsp --output geminids/data/SPICE/spp_recon_20181008_20190120_v001.bsp
-curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20180812_20181008_v001.bsp --output geminids/data/SPICE/spp_recon_20180812_20181008_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20210226_20210325_v001.bsp --output geminids/data/SPICE/spp_recon_20210226_20210325_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20210101_20210226_v001.bsp --output geminids/data/SPICE/spp_recon_20210101_20210226_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200802_20201016_v001.bsp --output geminids/data/SPICE/spp_recon_20200802_20201016_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20201016_20210101_v001.bsp --output geminids/data/SPICE/spp_recon_20201016_20210101_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200705_20200802_v001.bsp --output geminids/data/SPICE/spp_recon_20200705_20200802_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200505_20200705_v001.bsp --output geminids/data/SPICE/spp_recon_20200505_20200705_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200301_20200505_v001.bsp --output geminids/data/SPICE/spp_recon_20200301_20200505_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20200101_20200301_v001.bsp --output geminids/data/SPICE/spp_recon_20200101_20200301_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20190914_20200101_v001.bsp --output geminids/data/SPICE/spp_recon_20190914_20200101_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20190416_20190914_v001.bsp --output geminids/data/SPICE/spp_recon_20190416_20190914_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20190120_20190416_v001.bsp --output geminids/data/SPICE/spp_recon_20190120_20190416_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20181008_20190120_v001.bsp --output geminids/data/SPICE/spp_recon_20181008_20190120_v001.bsp
+# curl https://sppgway.jhuapl.edu/MOC/reconstructed_ephemeris/2018/spp_recon_20180812_20181008_v001.bsp --output geminids/data/SPICE/spp_recon_20180812_20181008_v001.bsp
 curl https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls --output geminids/data/SPICE/naif0012.tls
 curl https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de431_part-1.bsp --output geminids/data/SPICE/de431_part-1.bsp
 curl https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de431_part-2.bsp --output geminids/data/SPICE/de431_part-2.bsp
+cd geminids/data/SPICE
+wget https://www.dropbox.com/scl/fi/ayk4384rjqr9loa7g6vcu/small_kernels.zip\?rlkey=iei7x40aelfo5cuv2cqm0xygx\&dl=1
+unzip small_kernels.zip\?rlkey=iei7x40aelfo5cuv2cqm0xygx\&dl=1
+rm small_kernels.zip\?rlkey=iei7x40aelfo5cuv2cqm0xygx\&dl=1
